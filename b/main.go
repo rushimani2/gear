c1da2469d4303d46b487f7a623e55e03818971fd
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-05-10 13:46:38 +0530

    Update main.go

--- b/main.go
+++ b/main.go
@@ -20,7 +20,7 @@ func NewSillyDemo(scope constructs.Construct, id string) cdk8s.Chart {
 		jsii.String("sql"),
 		&AppProps{
 			Image:         jsii.String("vfarcic/silly-demo"),
-			Replicas:      jsii.Number(7),
+			Replicas:      jsii.Number(20),
 			Port:          jsii.Number(8080),
 			ContainerPort: jsii.Number(8080),
 		},
